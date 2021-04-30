fbf8978f5bc32a790743ae2d368778ae09ad01e8
Luca de Alfaro <luca@dealfaro.org>
2021-04-29 18:39:15 -0700

    vue version; works

--- static/js/index.js
+++ static/js/index.js
@@ -9,11 +9,19 @@ let init = (app) => {
     // This is the Vue data.
     app.data = {
         // Complete.
+        comment_list: [],
+        new_comment: "",
+    };
+
+    app.add_comment = function () {
+        app.vue.comment_list.push(app.vue.new_comment);
+        app.vue.new_comment = "";
     };
 
     app.methods = {
         // Complete.
-    }
+        add_comment: app.add_comment,
+    };
 
     // This creates the Vue instance.
     app.vue = new Vue({
